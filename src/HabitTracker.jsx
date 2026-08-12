25d8add0854803d5f45da3067efc468522511aa7
yuvrajshrirame <mail.yuvrajshrirame@gmail.com>
2026-08-12 18:59:37 +0530

    fix(db): add firebase keys

--- src/HabitTracker.jsx
+++ src/HabitTracker.jsx
@@ -38,13 +38,13 @@ import {
 
 // --- Firebase Configuration ---
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "___",
-  projectId: "___",
-  storageBucket: "___",
-  messagingSenderId: "___",
-  appId: "___",
-  measurementId: "___"
+  apiKey: "AIzaSyC7s8Cdd1a00UDfS-KdmowRDg6w7OZbNRo",
+  authDomain: "habit-tracker-ffd0a.firebaseapp.com",
+  projectId: "habit-tracker-ffd0a",
+  storageBucket: "habit-tracker-ffd0a.firebasestorage.app",
+  messagingSenderId: "13388020120",
+  appId: "1:13388020120:web:eefe7d66dcf24e1a43d3ed",
+  measurementId: "G-THY5Y1N00Z"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
